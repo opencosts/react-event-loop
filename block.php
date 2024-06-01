1b319ddcdc82a34b1097286d2976a2746cee038a
Open Costs <opencosts@gmail.com>
2024-06-01 22:39:09 +0530

    Block

---
+++ block.php
@@ -0,0 +1,16 @@
+<?php
+
+require "vendor/autoload.php";
+
+$loop = \React\EventLoop\Factory::create();
+
+$loop->addPeriodicTimer(1, function () {
+
+    echo "Hello World\n";
+});
+
+$loop->addTimer(1, function () {
+    sleep(5);
+});
+
+$loop->run();
